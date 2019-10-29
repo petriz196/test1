f6f7632ee14ac9b66036686f02d7a931082f668a
Petrit Rizvanolli <zero196@sbcglobal.net>
2019-10-29 17:19:50 -0500

    Update App.js
    a basic app that will take in a given list of objects and then group them all by a field then sort each group by another field. Filter out any objects where key data is null
    For example, given a list of items, separate them by item.listId then sort them by item.name, removing any items with a blank or null name.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.renderMethod.bind(this);
+  }
+
+  renderMethod () {
+    const pelements = [
+      { "id": 1, "label": "element1", "status": "status1" },
+      { "id": 2, "label": "element2","status": "status2" },
+      { "id": 3, "label": "element3", "status": "status1" },
+      { "id": 4, "label": "element4", "status": "status2" },
+      { "id": 5, "label": "element5", "status": "status3" },
+      { "id": 6, "label": "element6", "status": "status3" },
+      { "id": 7, "label": "element7", "status": null },
+      { "id": 8, "label": "element8", "status": "status1" },
+      { "id": 9, "label": "element9", "status": "status2" },
+      { "id": 10, "label": "element10", "status": null },
+    ];
+
+    const pgroups = [
+      { "name": "group1", "status" : ["status1"] },
+      { "name": "group2", "status" : ["status2"] },
+      { "name": "group3", "status" : ["status3"] }
+    ];
+
+
+    return React.createElement('div', {}, pgroups.map(
+      group => pelements.filter(element => group.status.includes(element.status)).map((element, index) => (
+        React.createElement('div', { key: 'p_' + index }, [
+          React.createElement('h1', { key: 'p_h1_' + index }, [element.label]),
+          React.createElement('p', { key: 'p_p_' + index }, [element.status])
+        ])
+      ))
+    ));
+  }
+
+
+  render () {
+    return this.renderMethod();
+  }
 }
 
 export default App;
